bb4ddb8e4a2478b91d3425ad46f9818badd16a0e
Nightsky1111 <Nightsky@ns.kr>
2021-05-29 11:22:28 +0900

    some style update is deployed

---
+++ SiteJSes/slider.js
@@ -0,0 +1,17 @@
+var sliderOptions = {
+    sliderId: "slider",
+    startSlide: 0,
+    effect: "series1",
+    effectRandom: false,
+    pauseTime: 2800,
+    transitionTime: 1200,
+    slices: 14,
+    boxes: 8,
+    hoverPause: 1,
+    autoAdvance: true,
+    thumbnailsWrapperId: "thumb_container_id",
+    m: false,
+    license: "mylicense"
+
+};
+var imageSlider = new mcImgSlider(sliderOptions);
