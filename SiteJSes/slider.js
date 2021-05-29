64cda7c596fa882516aa0dd4c4acbaa5a46c139e
Nightsky1111 <Nightsky@ns.kr>
2021-05-29 11:51:56 +0900

    some update for website formware

--- SiteJSes/slider.js
+++ SiteJSes/slider.js
@@ -1,103 +1,19 @@
-//current position
-var pos = 0;
-//number of slides
-var totalSlides = $('#slider-wrap ul li').length;
-//get the slide width
-var sliderWidth = $('#slider-wrap').width();
+var slideIndex = 0;
+showSlides();
 
-
-$(document).ready(function () {
-
-
-    /*****************
-     BUILD THE SLIDER
-    *****************/
-    //set width to be 'x' times the number of slides
-    $('#slider-wrap ul#slider').width(sliderWidth * totalSlides);
-
-    //next slide  
-    $('#next').click(function () {
-        slideRight();
-    });
-
-    //previous slide
-    $('#previous').click(function () {
-        slideLeft();
-    });
-
-
-
-    /*************************
-     //*> OPTIONAL SETTINGS
-    ************************/
-    //automatic slider
-    var autoSlider = setInterval(slideRight, 3000);
-
-    //for each slide 
-    $.each($('#slider-wrap ul li'), function () {
-
-        //create a pagination
-        var li = document.createElement('li');
-        $('#pagination-wrap ul').append(li);
-    });
-
-    //counter
-    countSlides();
-
-    //pagination
-    pagination();
-
-    //hide/show controls/btns when hover
-    //pause automatic slide when hover
-    $('#slider-wrap').hover(
-        function () { $(this).addClass('active'); clearInterval(autoSlider); },
-        function () { $(this).removeClass('active'); autoSlider = setInterval(slideRight, 3000); }
-    );
-
-
-
-});//DOCUMENT READY
-
-
-
-/***********
- SLIDE LEFT
-************/
-function slideLeft() {
-    pos--;
-    if (pos == -1) { pos = totalSlides - 1; }
-    $('#slider-wrap ul#slider').css('left', -(sliderWidth * pos));
-
-    //*> optional
-    countSlides();
-    pagination();
-}
-
-
-/************
- SLIDE RIGHT
-*************/
-function slideRight() {
-    pos++;
-    if (pos == totalSlides) { pos = 0; }
-    $('#slider-wrap ul#slider').css('left', -(sliderWidth * pos));
-
-    //*> optional 
-    countSlides();
-    pagination();
-}
-
-
-
-
-/************************
- //*> OPTIONAL SETTINGS
-************************/
-function countSlides() {
-    $('#counter').html(pos + 1 + ' / ' + totalSlides);
-}
-
-function pagination() {
-    $('#pagination-wrap ul li').removeClass('active');
-    $('#pagination-wrap ul li:eq(' + pos + ')').addClass('active');
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
