e03f06a451b19753194668ab5ddd60ec6c684ffc
Nightsky1111 <Nightsky@ns.kr>
2021-07-12 21:12:11 +0900

    debugged

--- backend/botTagchecck.js
+++ backend/botTagchecck.js
@@ -3,6 +3,8 @@ const NowBotTag = "별이#5136";
 
 let ask = prompt("현재 확인하시려는 봇의 태그를 아래 칸에 입력해주세요.");
 
+prompt("현재 확인하시려는 봇의 태그를 아래 칸에 입력해주세요.");
+
 if(ask === `${New_serviceTag}`){
     alert("해당 클라이언트는 서비스가 종료된 버전입니다 초대하기를 통해 새로운 봇을 초대해주세요!")
 } else if(ask === `${NowBotTag}`){
